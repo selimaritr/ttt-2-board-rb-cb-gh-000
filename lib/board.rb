3fad8fa7a0e621bd9d6ff47dd8ba2e6a8cb2bc73
Selim Ari <selim.ari1@ogr.sakarya.edu.tr>
2018-01-29 17:02:21 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 # Define the variable board below.
-row = [" ", " ", " "]
+row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 
 row[0]
@@ -10,5 +10,50 @@ row[1]
 
 row[2]
 
+
+row[3]
+
+
+row[4]
+
+
+row[5]
+
+
+row[6]
+
+
+row[7]
+
+
+row[8]
+
+
+row[9]
+
+
 row[1] = "X"
+
+
+row[2] = "O"
+
+
+row[3] = "X"
+
+
+row[4] = "O"
+
+
+row[5] = "X"
+
+row[6] = "O"
+
+
+row[7] = "X"
+
+
+row[8] = "O"
+
+
+row[9] = "X"
 row
